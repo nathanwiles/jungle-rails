2d103e4d5ff17f8bcde96ae714a71efcdd9d7f07
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-10-23 23:19:52 -0400

    define instance variables for dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
   def show
+    @product_count = { value: Product.count, name: 'Product count' }
+    @category_count = { value: Category.count, name: 'Category count' }
+    @metrics = [@product_count, @category_count]
   end
 end
