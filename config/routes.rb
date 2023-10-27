760b7e5f91b3f8a9cb73f4d961ee8ca16c61bcb7
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-10-27 15:06:38 -0400

    products route to use index, add categories route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-
   root to: 'products#index'
 
-  resources :products, only: [:index, :show]
+  resources :products, only: %i[index show]
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
@@ -10,14 +9,16 @@ Rails.application.routes.draw do
     post   :remove_item
   end
 
-  resources :orders, only: [:create, :show]
+  resources :orders, only: %i[create show]
 
   namespace :admin do
-    root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
+    root to: 'dashboard#index'
+    resources :products, except: %i[edit update show]
+    resources :categories, only: %i[new create index]
   end
 
   get 'about' => 'about#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
