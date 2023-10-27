78f3ef7960572dc3c326fc57179c124d40e24afb
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-10-27 15:04:12 -0400

    change from show to index

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
-  def show
+  def index
     @product_count = { value: Product.count, name: 'Product count' }
     @category_count = { value: Category.count, name: 'Category count' }
     @metrics = [@product_count, @category_count]
