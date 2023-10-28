9246ac322ef776ad5d9665f348aca01ef7e5d6c0
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-10-27 20:46:55 -0400

    generate user model

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,9 @@ Rails.application.routes.draw do
     resources :categories, only: %i[new create index]
   end
 
-  get 'about' => 'about#index'
+  get '/about' => 'about#index'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
