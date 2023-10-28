ba76e8913dd0ec3ca5f26521a3a5fed2af8cb506
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-10-27 21:20:30 -0400

    define the create method for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
+# frozen_string_literal: true
+
+# Users controller
 class UsersController < ApplicationController
+  def new; end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
