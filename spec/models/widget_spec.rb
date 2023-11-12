c3d867df37d1769b7ef4e08cd5c6b6adbe55d9d8
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-12 18:29:41 -0500

    write example tests

--- spec/models/widget_spec.rb
+++ spec/models/widget_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 describe '#id' do
   it 'should not exist for new records' do
-    @widget
+    @widget = Widget.new
+    expect(@widget.id).to be_nil
+  end
+
+  it 'should be auto-assigned by AR for saved records' do
+    @widget = Widget.new
+    # we use bang here because we want it to fail if save fails (due to validations)
+    @widget.save!
+
+    expect(@widget.id).to be_present
   end
 end
