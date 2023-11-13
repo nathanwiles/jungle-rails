11b67eace1f8bd9ad6dad069c2df13c12e285d3d
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-12 22:46:48 -0500

    limit to 1 expect per

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,25 +12,22 @@ RSpec.describe Product, type: :model do
 
     it 'should validate name' do
       @product = Product.new(name: nil, price: 100, quantity: 1, category: @category)
-      expect(@product).to_not be_valid
       expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'should validate price' do
       @product = Product.new(name: 'test', price: nil, quantity: 1, category: @category)
-      expect(@product).to_not be_valid
-      expect(@product.errors.full_messages).to include("Price can't be blank")
+
+      epect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
     it 'should validate quantity' do
       @product = Product.new(name: 'test', price: 100, quantity: nil, category: @category)
-      expect(@product).to_not be_valid
       expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'should validate category' do
       @product = Product.new(name: 'test', price: 100, quantity: 1, category: nil)
-      expect(@product).to_not be_valid
       expect(@product.errors.full_messages).to include("Category can't be blank")
     end
   end
