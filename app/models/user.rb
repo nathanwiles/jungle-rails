61fe25e9df1e5f13b6dd835db757bfd2a8dbce5a
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-11-12 21:48:25 -0500

    create authenticate_with_credentials satisfy tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
 
+  def self.authenticate_with_credentials(email, password)
+    User.find_by_email(email.downcase.strip)
+        .try(:authenticate, password) || nil
+  end
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 8 }
